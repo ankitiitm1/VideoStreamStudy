7826908a144a17c08290ba05e41b90ec88ce5795
Ankit Pandey <ankit.iitm1@gmail.com>
2023-01-02 18:09:49 +0530

    firstcommit

---
+++ cssplaylist/logic.js
@@ -0,0 +1,20 @@
+function openCourse(evt, courseName) {
+    // Declare all variables
+    var i, tabcontent, tablink;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablink = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablink.length; i++) {
+        tablink[i].className = tablink[i].className.replace(" active", "");
+    }
+
+    // // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(courseName).style.display = "block";
+    // evt.currentTarget.className += " active";
+}
